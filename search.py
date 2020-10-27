54d96b61f3802c1417be74113e9293efe1ebf65c
Vittorio Raoul Tavolaro <vittorio.tavolaro@cern.ch>
2020-10-27 14:43:40 +0100

    scroll into view

--- search.py
+++ search.py
@@ -38,7 +38,9 @@ def searchLine(myline, url="http://cti.voa.gov.uk/cti/inits.asp", browser_option
     while True:
         a=''
         try:
+#            //*[@id="Content"]/div/div[2]/div/div[2]/div/a
             a = driver.find_element_by_class_name('next')#driver.execute_script("Next();")
+            driver.execute_script('arguments[0].scrollIntoView(true)', a)
             a.click()
             time.sleep(2)
         except selenium.common.exceptions.NoSuchElementException:#selenium.common.exceptions.JavascriptException:
